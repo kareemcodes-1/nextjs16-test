a7e7ac169fa5f71a4238b63e9eea14065fe435af
Kareem Braimoh <kareembrai222@gmail.com>
2026-04-04 12:40:47 +0100

    animation problem

--- app/(pages)/(home)/page.tsx
+++ app/(pages)/(home)/page.tsx
@@ -1,12 +1,8 @@
 // "use client";
 // import { useEffect } from "react";
 import About from "../../components/about";
-import Collections from "../../components/collections/collections";
 import Hero from "../../components/hero";
 import Testimonials from "../../components/testimonials";
-// import LenisProvider from "../../providers/lenis-provider";
-import BestSellers from "../../components/best-sellers/best-sellers";
-import FeaturedProducts from "@/app/components/products/featured-products";
 
 
 
